0a819bc42e68bebcec6f986458d1fa1b652937c9
Your Name <shishir323@gmail.com>
2018-11-13 23:05:10 +0600

    Initial Commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,8 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+import Buefy from 'buefy';
+
+Vue.use(Buefy);
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
